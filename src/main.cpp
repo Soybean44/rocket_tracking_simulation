5f75db88c5938986fb4d821248dd1bd6d26be5e9
Sovereign Shahid <sovereignshahid@gmail.com>
2025-04-29 19:49:36 -0600

    fixed weird rendering bugs so that the movement shows up properly

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
+#include <algorithm>
 #include <cmath>
+#include <iostream>
 #include <raylib.h>
+#include <raymath.h>
 #include <rcamera.h>
 
 class Cube {
@@ -14,37 +17,42 @@ public:
 
   void render() {
     DrawCubeV(pos, size, color);
-    DrawCubeWiresV(pos, size, border_color);
+    DrawCubeWiresV(pos, Vector3Add(size, Vector3{0.01, 0.01, 0.01}),
+                   border_color);
   }
 };
 
 constexpr float delta = 1;
-constexpr float step = 0.1;
+constexpr float step = 0.01;
 
 void TrackCube(Camera *cam, Vector3 cubePos, Vector2 targetPos) {
   bool isNotCenter;
+  Vector2 posError, cubeScreenPos;
+  // Keep adjusting the camera while the rocket isnt in the center
+  // It is in the center when no adjustments ahve been made
   do {
     isNotCenter = false;
-    Vector2 cubeScreenPos = GetWorldToScreen(cubePos, *cam);
-    Vector2 posError = {.x = cubeScreenPos.x - targetPos.x,
-                        .y = cubeScreenPos.y - targetPos.y};
+    cubeScreenPos = GetWorldToScreen(cubePos, *cam);
+    posError = {.x = cubeScreenPos.x - targetPos.x,
+                .y = cubeScreenPos.y - targetPos.y};
 
     if (posError.y < -delta) {
-      CameraPitch(cam, step * DEG2RAD, true, false, false); // Move 1 step
+      CameraPitch(cam, step * DEG2RAD, true, false, true); // Move 1 step
       isNotCenter = true;
     }
     if (posError.y > delta) {
-      CameraPitch(cam, -step * DEG2RAD, true, false, false); // Move 1 step
+      CameraPitch(cam, -step * DEG2RAD, true, false, true); // Move 1 step
       isNotCenter = true;
     }
     if (posError.x < -delta) {
-      CameraYaw(cam, step * DEG2RAD, true); // Move 1 step
+      CameraYaw(cam, step * DEG2RAD, false); // Move 1 step
       isNotCenter = true;
     }
     if (posError.x > delta) {
-      CameraYaw(cam, -step * DEG2RAD, true); // Move 1 step
+      CameraYaw(cam, -step * DEG2RAD, false); // Move 1 step
       isNotCenter = true;
     }
+    std::cout << posError.y << "\n";
   } while (isNotCenter);
 }
 
@@ -56,22 +64,25 @@ int main(void) {
   Vector2 targetPos = {(float)(screenWidth / 2), (float)(screenHeight / 2)};
 
   Camera3D camera = {0};
-  camera.position = (Vector3){0.0f, 10.0f, 10.0f};
+  camera.position = (Vector3){0.0f, 10.0f, 20.0f};
   camera.up = (Vector3){0.0f, 1.0f, 0.0f};
   camera.fovy = 45.0f;
   camera.projection = CAMERA_PERSPECTIVE;
 
   Cube cube(Vector3{0, 1, 0}, Vector3{2, 2, 2}, RED, BLACK);
   Vector2 cubeScreenPos;
+  // start by making the camera look at the rocket directly
   camera.target = cube.pos;
   SetTargetFPS(60);
 
+  float t = 0;
   while (!WindowShouldClose()) {
     float dt = GetFrameTime();
     cubeScreenPos = GetWorldToScreen(cube.pos, camera);
 
-    cube.pos.y += 5 * dt;                    // Move the rocket up
-    cube.pos.x = 3 * (float)sin(cube.pos.y); // Move the rocket horizontally
+    t += 5 * dt;
+    cube.pos.y = t;                     // Move the rocket up
+    cube.pos.x = 9 * (float)sin(t / 2); // Move the rocket horizontally
 
     TrackCube(&camera, cube.pos, targetPos);
 
@@ -79,7 +90,8 @@ int main(void) {
     ClearBackground(SKYBLUE);
     BeginMode3D(camera);
 
-    DrawPlane(Vector3{0, 0, 0}, Vector2{100, 100}, GRAY);
+    // Draw ground
+    DrawPlane(Vector3{0, 0, 0}, Vector2{50, 50}, DARKGREEN);
     cube.render();
 
     EndMode3D();
@@ -87,7 +99,10 @@ int main(void) {
                         .y = cubeScreenPos.y - targetPos.y};
     const char *pos_str =
         TextFormat("Pos Error: (%f, %f)", posError.x, posError.y);
-    DrawText(pos_str, 10, 10, 24, BLACK);
+    const char *cam_str =
+        TextFormat("Camera Position: (%f, %f, %f)", camera.position.x,
+                   camera.position.y, camera.position.z);
+    DrawText(cam_str, 10, 10, 24, BLACK);
     EndDrawing();
   }
   CloseWindow();
